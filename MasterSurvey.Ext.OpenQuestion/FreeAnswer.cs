09cc9bf1e7bda5d42bfd99545d3cd66d956fc7a9
Nementon <gueux666@gmail.com>
2014-07-14 06:44:50 +0200

    Add FreeQuestion plugin (In progress)

---
+++ MasterSurvey.Ext.OpenQuestion/FreeAnswer.cs
@@ -0,0 +1,26 @@
+﻿using MasterSurvey.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterSurvey.Ext.FreeQuestion
+{
+    public class FreeAnswer : BaseAnswer
+    {
+        String _answer = String.Empty;
+
+        public FreeAnswer(IQuestion lkQ) : base(lkQ) { }
+
+        public String Answer { get { return _answer; } set { _answer = value; } }
+
+        public override bool IsValid()
+        {
+            if (Answer == String.Empty && !base.LinkedQuestion.AllowEmptyAnswer) {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ MasterSurvey.Ext.OpenQuestion/FreeQuestion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MasterSurvey.Core;
+
+namespace MasterSurvey.Ext.FreeQuestion
+{
+    public class FreeQuestion : BaseQuestion
+    {
+        public override Type GetAnswerType()
+        {
+            return typeof(FreeAnswer);
+        }
+    }
+}
